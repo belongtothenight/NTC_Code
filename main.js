8f411131dfe9061ab8bf75fbac948e4bfde9bd5e
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-01-01 20:09:54 +0800

    update

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-let aes_enc = require('./lib/aes_enc.js');
-let aes_dec = require('./lib/aes_dec.js');
 let aeslib = require('./lib/aes.js');
 console.log('main.js')
 
-const plaintext = 'hello world 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111';
 const key = '12345678901234567890123456789012';
-const iv = '1234567890123456'; // 16 bytes (skipped)
-const mode = 'ECB'; // ECB, CBC, CFB, OFB, CTR (skipped)
-const padding = 'PKCS7'; // PKCS7, ISO10126, ISO7816, Zeros, ANSIX923, None (skipped)
 const blockSize = 128; // 128, 192, 256
-const outputType = 'Hex'; // Hex, Base64, String (skipped)
+const iv = false; // 16 bytes (skipped)
+const mode = 'ECB'; // ECB, CBC, CFB, OFB, CTR (skipped)
+const padding = false; // PKCS7, ISO10126, ISO7816, Zeros, ANSIX923, None (skipped)
+const outputType = 'String'; // Hex, Base64, String (skipped)
 
-var aes = new aeslib.AES();
+var aes = new aeslib.AES(key, blockSize, iv, mode, padding, outputType);
+
+const plaintext = 'hello world 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111';
