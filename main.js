7470993493be5d8a485a6b7fe7d1cb774e3500ae
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-01-01 19:17:30 +0800

    update

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let aes_enc = require('./lib/aes_enc.js');
-let aec_dec = require('./lib/aes_dec.js');
+let aes_dec = require('./lib/aes_dec.js');
+let aeslib = require('./lib/aes.js');
 console.log('main.js')
 
-const plaintext = 'hello world';
+const plaintext = 'hello world 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111';
 const key = '12345678901234567890123456789012';
 const iv = '1234567890123456'; // 16 bytes (skipped)
 const mode = 'ECB'; // ECB, CBC, CFB, OFB, CTR (skipped)
@@ -10,5 +11,4 @@ const padding = 'PKCS7'; // PKCS7, ISO10126, ISO7816, Zeros, ANSIX923, None (ski
 const blockSize = 128; // 128, 192, 256
 const outputType = 'Hex'; // Hex, Base64, String (skipped)
 
-aes_enc.aes_enc(plaintext, key, iv, mode, padding, blockSize, outputType);
-aec_dec.aes_dec();
+var aes = new aeslib.AES();
