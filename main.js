2e9c6ca577f4e03e35fce05521c5d9bc941bf256
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-01-02 19:03:14 +0800

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let aeslib = require('./lib/aes.js');
 
 // const key = 1234567890123456;
-const key = '2b28ab097eaef7cf15d2154f16a6883c';
+const key = '2b7e151628aed2a6abf7158809cf4f3c';
 const blockSize = 128; // 128, 192, 256
 const iv = false; // 16 bytes (skipped)
 const mode = 'ECB'; // ECB, CBC, CFB, OFB, CTR (skipped)
@@ -9,8 +9,8 @@ const padding = false; // PKCS7, ISO10126, ISO7816, Zeros, ANSIX923, None (skipp
 const outputType = 'String'; // Hex, Base64, String (skipped)
 
 var aes = new aeslib.AES(key, blockSize, iv, mode, padding, outputType);
-console.log('key: ' + aes.args.key);
-console.log('expandedKey: ' + aes.args.w);
+// console.log('key: ' + aes.args.key);
+console.log('Initialized AES');
 
-const plaintext = 'hello world 11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111';
+const plaintext = '3243f6a8885a308d313198a2e0370734';
 aes.encrypt(plaintext);
