5eed678a6a8f72898e4a21ff923fa4efa6103df2
belongtothenight <106849935+belongtothenight@users.noreply.github.com>
2023-01-03 18:12:58 +0800

    upddate

--- build.js
+++ build.js
@@ -1,2 +1,12 @@
 // move all files
+let path1 = "./webpage/";
+let path2 = "./public/";
+
+
+var myObject = new ActiveXObject("Scripting.FileSystemObject");
+var myFolder = myObject.GetFolder(path1);
+
+myFolder.Copy(path2, false);
+
+
 // check files
